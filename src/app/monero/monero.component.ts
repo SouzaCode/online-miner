b6a56b73398fcc811ddc017f0bd83e028d416a7b
Rodrigo Ferraz Souza <rodrigo.souza289@etec.sp.gov.br>
2020-06-24 04:47:46 -0300

    initial commit

---
+++ src/app/monero/monero.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-monero',
+  templateUrl: './monero.component.html',
+  styleUrls: ['./monero.component.scss']
+})
+export class MoneroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
